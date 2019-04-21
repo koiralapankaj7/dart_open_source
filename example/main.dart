95536f92b69c12c036cc28086ea71dcc8a2908d3
cricpunk <koiralapankaj007@gmail.com>
2019-04-21 10:15:54 +0545

    completed

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,19 @@
-import 'package:pk_prompter/src/terminal.dart';
+import 'package:pk_prompter/pk_prompter.dart';
 
 void main(List<String> args) {
-  Terminal terminal = Terminal();
-  terminal.clearScreen();
-  terminal.printPrompt("Hello there");
+  final prompter = Prompter();
+
+  final String question1 = 'Which color do you want?';
+  final options1 = [
+    Option('I want red', '#F00'),
+    Option('I want blue', '#00F'),
+    Option('I want green', '#0F0'),
+  ];
+
+  final String question2 = 'Do you like this lib?';
+
+  String colorCode = prompter.askMultipleChoice(question1, options1);
+  bool answer = prompter.askBinary(question2);
+  print(colorCode);
+  print(answer);
 }
